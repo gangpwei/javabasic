302400bd442811a4b48fa13e5037252bf3fa8203
weigangpeng <imwgp@163.com>
2022-11-22 11:48:05 +0800

    用栈实现队列，要求线程安全

--- src/main/java/com/wgp/MyQueue.java
+++ src/main/java/com/wgp/MyQueue.java
@@ -4,39 +4,69 @@ import java.util.Stack;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ * 用栈实现队列，要求线程安全
  * @author gangpeng.wgp
  * @date 2022/11/22 上午9:17
  */
-public class MyQueue {
+public class MyQueue<E> {
 
-    private Stack<Object> stackA = new Stack<Object>();
+    //入栈
+    private Stack<E> inStack = new Stack<E>();
 
-    private Stack<Object> stackB = new Stack<Object>();
+    //出栈
+    private Stack<E> outStack = new Stack<E>();
 
+    //锁
     private ReentrantLock lock = new ReentrantLock();
 
-    public void add(Object obj) {
+    /**
+     * 入队列
+     * @param obj
+     */
+    public void offer(E obj) {
         try {
             lock.lock();
-            stackA.push(obj);
-            while (!stackA.empty()){
-                stackB.push(stackA.pop());
-            }
-        } finally {
-            lock.lock();
-        }
-    }
 
-    public Object offer(){
-        try {
-            lock.lock();
-            return stackB.pop();
+            System.out.println("入队列：" + obj);
+
+            //入栈之前，把出栈队列的元素还原回来
+            while (!outStack.empty()) {
+                inStack.push(outStack.pop());
+            }
+
+            //把新的元素入栈
+            inStack.push(obj);
         } finally {
             lock.unlock();
         }
     }
 
-    public int size(){
-        return stackB.size();
+    /**
+     * 出队列
+     * @return
+     */
+    public E poll() {
+        try {
+            lock.lock();
+
+            //出栈前，把入栈元素倒排放到出栈
+            while (!inStack.empty()) {
+                outStack.push(inStack.pop());
+            }
+
+            E obj = outStack.pop();
+            System.out.println("   出队列：" + obj);
+            return obj;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 队列是否为空
+     * @return 队列是否为空
+     */
+    public boolean isEmpty() {
+        return outStack.isEmpty() && inStack.isEmpty();
     }
 }
---
+++ src/test/java/com/wgp/MyQueueTest.java
@@ -0,0 +1,94 @@
+package com.wgp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @author gangpeng.wgp
+ * @date 2022/11/22 上午10:29
+ */
+public class MyQueueTest extends TestCase {
+
+    @Test
+    public void testPoll() {
+        final MyQueue<String> queue = new MyQueue();
+
+        List<Callable> taskList = new ArrayList<Callable>();
+
+
+        taskList.add(new Callable() {
+            @Override
+            public Object call() {
+                for (int i = 1; i <= 5; i++) {
+                    queue.offer("A" + i);
+                    sleep(200);
+                }
+                return null;
+            }
+        });
+
+        taskList.add(new Callable() {
+            @Override
+            public Object call() {
+                for (int i = 1; i <= 5; i++) {
+                    queue.offer("B" + i);
+                    sleep(100);
+                }
+                return null;
+            }
+        });
+
+        taskList.add(new Callable() {
+            @Override
+            public Object call() {
+                for (int i = 1; i <= 2; i++) {
+                    queue.poll();
+                    sleep(400);
+                }
+                return null;
+            }
+        });
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        List<Future> futures = new ArrayList<Future>();
+        for (Callable callable : taskList) {
+            Future future = executorService.submit(callable);
+            futures.add(future);
+        }
+
+        for (Future future : futures) {
+            try {
+                future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        System.out.println("====队列元素");
+        while(!queue.isEmpty()) {
+            queue.poll();
+        }
+
+    }
+
+    private void sleep(int value) {
+        try {
+            Thread.sleep(value);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
